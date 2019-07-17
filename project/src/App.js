009a0c73d56272b6e38e271e4c9becf8c3bd9833
James Kenniff <jameskenniff@Jamess-MacBook-Air-3.local>
2019-07-17 11:15:25 -0400

    homepage small change git push

--- project/src/App.js
+++ project/src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
   }
   render(){
     return(
+      <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      </head>
       <div>
         <h1 class ="header1">
         activst
