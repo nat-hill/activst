5ce3a86cfe86c8cea0ffcd04130acb11ac4eef34
James Kenniff <jameskenniff@Jamess-MacBook-Air-3.local>
2019-07-22 13:28:28 -0400

    its beautiful

--- project/src/App.js
+++ project/src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 15,
+            minZoom: 12,
             id: 'mapbox.streets',
             accessToken: 'pk.eyJ1IjoiY3VzdW1tZXIiLCJhIjoiY2p5NXc5cXhwMDFxeTNmbzhwNWpsZTRibSJ9.204smoZZqhejVVBy7oiHfg'
         }).addTo(mymap);
@@ -52,7 +53,7 @@ class App extends Component {
               <a class="nav" href="/login">Log In</a>
             </li>
             <li class = "listb">
-              <a class="nav" href="protest.js">Create a Protest</a>
+              <a class="nav" href="/protest.js">Create a Protest</a>
             </li>
             <li class ="listb">
               <a class="nav" href="myProfile">My Profile</a>
