caeb585799cd2f1f1e8ceeda12f6c64a60214738
William Hill <wnhill13@gmail.com>
2019-07-22 10:39:47 -0400

    MAP DONE

--- project/src/App.js
+++ project/src/App.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
 import AppComponent from './AppComponent';
 import './App.css';
+var L = require("leaflet");
 
 class App extends Component {
   constructor(){
     super();
     this.state = {};
   }
+
+
+  componentDidMount(){
+  var mymap = L.map('mapid').setView([40.7831, -73.9712],9);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoiY3VzdW1tZXIiLCJhIjoiY2p5NXc5cXhwMDFxeTNmbzhwNWpsZTRibSJ9.204smoZZqhejVVBy7oiHfg'
+        }).addTo(mymap);
+  }
+
   render(){
     return(
       <div>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+    crossorigin=""/>
+
         <h1 class ="header1">
         activst
         </h1>
@@ -36,9 +54,13 @@ class App extends Component {
             <li class="listb">
               <a class="nav" href="aboutUs">About Us</a>
             </li>
+
           </ul>
-          <script src="./NewMap.js"></script>
+
+          <div id="mapid"></div>
+
         </div>
+
       </div>
     );
 
