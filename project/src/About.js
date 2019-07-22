42d367a78a594268dccd8eda37a97438cbbf1b26
unknown <nano>
2019-07-22 15:20:58 -0400

    fixed

--- project/src/About.js
+++ project/src/About.js
@@ -22,9 +22,9 @@ class About extends Component {
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
           </head>
           <body>
+            <Navbar />
             <div class="container">
               <div class="masthead">
-                <Navbar />
               </div>
               <div class="jumbotron">
                 <h1>Our mission</h1>
